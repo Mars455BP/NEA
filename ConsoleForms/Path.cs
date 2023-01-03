49e392a65e82e6d3282580f2a0551005322e9cc9
Mars455BP <119410347+Mars455BP@users.noreply.github.com>
2023-01-03 11:06:35 +0000

    Update Path.cs

--- ConsoleForms/Path.cs
+++ ConsoleForms/Path.cs
@@ -39,6 +39,26 @@ namespace ConsoleForms
                 case "Apples":
                     aisleNum = 2;
                     break;
+                case "Chopped Tomatoes":
+                case "Baked Beans":
+                case "Chilli Powder":
+                case "Garlic":
+                    aisleNum = 3;
+                    break;
+                case "Frozen Pizza":
+                case "Ice Cream":
+                    aisleNum = 4;
+                    break;
+                case "Dog Toys":
+                case "Dog Food":
+                case "Cat Food":
+                    aisleNum = 5;
+                    break;
+                default:
+                    aisleNum = 0;
+                    Console.WriteLine("Error- item not found");
+                    break;
+
             }
             return 0;
         }
